a151dc8240a1318b6ce21f456ee5cce99581b78e
Irwin Williams <irwin.williams@teleios-systems.com>
2017-10-26 21:41:18 -0400

    fixing a spacing issue

--- Workshop/10. VM Scaling/resources/storageconfig.py
+++ Workshop/10. VM Scaling/resources/storageconfig.py
@@ -1,5 +1,4 @@
-storage =
-{'storage_account_name': 'scalinglab1',
- 'storage_account_key': 'dzk6lVRNytQXEPJ8n4QcecmQgwe51hZG+tGv+m/rJJt+axknXWJxt9jm3TQcBELugoHFPftvMC2MOaFh5iiSsQ==',
- 'container_name': 'distances',
- 'blob_name': 'log.txt'}
+storage = {'storage_account_name': 'scalinglab1',
+           'storage_account_key': 'dzk6lVRNytQXEPJ8n4QcecmQgwe51hZG+tGv+m/rJJt+axknXWJxt9jm3TQcBELugoHFPftvMC2MOaFh5iiSsQ==',
+           'container_name': 'distances',
+           'blob_name': 'log.txt'}
