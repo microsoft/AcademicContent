3df8ba911cc9ac84ecf5d13a14ebbca1611ade21
Shana Matthews <shanama@microsoft.com>
2018-03-20 10:44:33 -0700

    Move other labs to draft folder

---
+++ Educator Content/Content in draft/Azure IoT/Adafruit_Feather_IoTSuite/Blink/Blink/Blink.ino
@@ -0,0 +1,13 @@
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin 13 as an output.
+  pinMode(13, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);              // wait for a second
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);              // wait for a second
+}
