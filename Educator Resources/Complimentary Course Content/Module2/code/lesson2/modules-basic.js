93c32fbb50deb356d4466d39d0c32b9477af2299
Jim Bennett <jim.bennett@microsoft.com>
2020-03-24 12:32:44 -0700

    Cleaning all the things!
    Deleting all the content as part of #242

--- Educator Resources/Complimentary Course Content/Module2/code/lesson2/modules-basic.js
+++
@@ -1,6 +0,0 @@
-// The rest of the code is in the increment.js and
-// math.js files holding those modules.
-
-var inc = require('./increment').increment
-var a = 1
-console.log(inc(a)) // This will return 2
--- Educator Resources/Scripts/Bulk-Sandbox-Deployment-Automation-Bash/modules/deep-learningtoolkit/install.ps1
+++
@@ -1,6 +0,0 @@
-Import-Module BitsTransfer
-
-cd C:\Windows\temp
-Start-BitsTransfer -Source https://deeplearningtoolkit.blob.core.windows.net/assets/install.latest.ps1 -Destination ".\install.latest.ps1"
-
-.\install.latest.ps1
