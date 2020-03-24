ad205355c6fdd770caca0653ee6b91f89f057b56
Jim Bennett <jim.bennett@microsoft.com>
2020-03-24 13:42:36 -0700

    Adding vewpress site

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+	title: 'Academic Content for Students and Researchers',
+	description: 'Curated workshops, articles, curriculum and more for teachers and students',
+	themeConfig: {
+		logo: '/assets/logo.png',
+		search: true,
+		lastUpdated: 'Last Updated',
+		editLinks: true,
+		editLinkText: 'Help us improve this page!',
+		nav: [
+			{ text: 'Home', link: '/' },
+			{ text: 'Code of Conduct', link: '/CODE_OF_CONDUCT' },
+			{ text: 'Contribute', link: '/CONTRIBUTING' },
+		],
+		sidebar: [
+			'/content/learning-content',
+			'/content/hackathon-content',
+			'/content/event-content',
+			'/content/k-12-content',
+		],
+	},
+};
