933f5fa5a2604a790c4000ec762f93acf82107ed
Jim Bennett <jim.bennett@microsoft.com>
2020-03-26 10:15:57 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env sh
 
-if [ "$1" == "" ] || [ "$2" == "" ] || [ "$3" == "" ]; then
-    echo "Usage: ./deploy.sh <org> <username> <password>"
-    echo "<org> is the name of the GitHub org to push to"
-    echo "<username> is the username of the GitHub user pushing this"
-    echo "<password> is the password of the GitHub user pushing this"
+if [ "$1" == "" ]; then
+    echo "Usage: ./deploy.sh <org>"
+    echo "Where <org> is the name of the GitHub org to push to"
     exit -1
 fi
 
@@ -24,6 +22,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f https://$2:$3@github.com/$1/AcademicContent.git master:gh-pages
+git push -f git@github.com:$1/AcademicContent.git master:gh-pages
 
 cd -
